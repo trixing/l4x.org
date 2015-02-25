30bd906759abdfadcff1c7a5782d890d7d37fd0d
Jan Dittmer <jdi@l4x.org>
2015-02-25 01:43:40 -0500

    remove another unused file

--- add-meta-date.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for f in `find entries/ -type f -name '*.txt' -o -name '*.rst'`; do
-	if ! test -f "$f"; then continue; fi
-	echo $f
-	DATE=$(svn info "$f" | egrep "^Last Changed Date" | cut -f4-5 -d' ')
-	META="[[!meta date=\"$DATE\"]]"
-	echo "$META" >> "$f"
-done
